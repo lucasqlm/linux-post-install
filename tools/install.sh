4887617cf5b463b81eabac051cc98b3dafb16a4b
Lucas Queiroz <lucas@incognia.com>
2021-05-30 21:31:39 -0300

    Changes to fedora 34

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl --location --output post-install.zip https://github.com/lucasqlm/linux-post-install/archive/feat/fedora-33.zip \
+curl --location --output post-install.zip https://github.com/lucasqlm/linux-post-install/archive/feat/fedora-34.zip \
   && unzip -qo post-install.zip -d linux-post-install\
   && rm post-install.zip
 
 pip install ansible
 
-cd linux-post-install/linux-post-install-feat-fedora-33
+cd linux-post-install/linux-post-install-feat-fedora-34
 ./start.sh
