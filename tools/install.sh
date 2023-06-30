9194b6995143eaf96d1632259a82d6b6c060aae3
Lucas Queiroz <lucasqlm@gmail.com>
2023-06-30 01:33:17 -0300

    Add debian & updates

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-curl --location --output post-install.zip https://github.com/lucasqlm/linux-post-install/archive/feature/general-install.zip \
-  && unzip -qo post-install.zip -d linux-post-install\
-  && rm post-install.zip
+OS=$(lsb_release -si)
+VER=$(lsb_release -sr)
 
-pip install ansible
+case $OS in
+Debian | Ubuntu)
+    wget -O post-install.zip https://github.com/lucasqlm/linux-post-install/archive/feature/general-install.zip \
+      && unzip -qo post-install.zip -d linux-post-install\
+      && rm post-install.zip
 
-cd linux-post-install/linux-post-install-feature-general-install
-./start.sh
+    sudo apt update && sudo apt install -y python3-pip
+    pip install ansible
+
+    cd linux-post-install/linux-post-install-feature-general-install
+    PATH=$HOME/.local/bin:$PATH ./start.sh
+    ;;
+Fedora)
+    curl --location --output post-install.zip https://github.com/lucasqlm/linux-post-install/archive/feature/general-install.zip \
+      && unzip -qo post-install.zip -d linux-post-install\
+      && rm post-install.zip
+
+    pip install ansible
+
+    cd linux-post-install/linux-post-install-feature-general-install
+    ./start.sh
+    ;;
+*)
+    echo "OS/Distro not supported"
+    ;;
+esac
+
+
