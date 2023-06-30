a86b91598071363b8d73970a486276a83d8b61ba
Lucas Queiroz <lucasqlm@gmail.com>
2023-06-30 02:00:18 -0300

    fix debian install script

--- tools/install.sh
+++ tools/install.sh
@@ -11,7 +11,7 @@ Debian | Ubuntu)
 
     sudo apt update && sudo apt install -y ansible
 
-    cd linux-post-install/linux-post-install-feature-general-install
+    cd linux-post-install/linux-post-install-feature-master
     PATH=$HOME/.local/bin:$PATH ./start.sh
     ;;
 Fedora)
@@ -21,7 +21,7 @@ Fedora)
 
     pip install ansible
 
-    cd linux-post-install/linux-post-install-feature-general-install
+    cd linux-post-install/linux-post-install-feature-master
     ./start.sh
     ;;
 *)
